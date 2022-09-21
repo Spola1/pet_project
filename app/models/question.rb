d05e302f7a4b4b5b31056094710ea5faeac2d768
Spola1 <1111.22224@mail.ru>
2022-09-21 11:25:51 +0300

    setup models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,24 @@
 class Question < ApplicationRecord
   include Commentable
-  
+
   has_many :answers, dependent: :destroy
   belongs_to :user
+  has_many :question_tags, dependent: :destroy
+  has_many :tags, through: :question_tags
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: { minimum: 5 }
+
+  scope :all_by_tags, ->(tag_ids) do
+    questions = includes(:user)
+    if tag_ids
+      questions = questions.includes(question_tags: :tag).joins(:tags).where(tags: tag_ids)
+    else
+      questions = questions.includes(:question_tags, :tags)
+    end
+
+    questions.order(created_at: :desc)
+  end
 
   def created_at_format
     created_at.strftime('%Y-%m-%d %H:%M:%S')
