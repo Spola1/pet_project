790f2188eb2e8e033c6bee042a709d3f3887f7c2
Spola1 <1111.22224@mail.ru>
2023-01-17 01:41:58 +0300

    update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ user3 = User.create(email:'admin@admin.com', name:'admin', nickname:'admin',
   title = Faker::Hipster.word
   body = "#{Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)} 
     ##{Faker::Hipster.word} ##{Faker::Hipster.word}"
-  Question.create(title: title, body: body, user_id: user3.id, cached_votes_score: rand(2..15))
+  question = Question.create(title: title, body: body, user_id: user3.id)
 end
 
 30.times do
