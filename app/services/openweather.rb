af9981b29e670b4a4331d615002541601a15f93d
Spola1 <1111.22224@mail.ru>
2023-01-21 14:31:37 +0300

    update weather service

--- app/services/openweather.rb
+++ app/services/openweather.rb
@@ -1,13 +1,8 @@
 module Openweather
   class Search
     def self.by_location(location)
-      begin
-        @parsed_response = 
-        HTTParty.get('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY'])
-      rescue URI::InvalidURIError
-        @parsed_response = 
-        HTTParty.get('https://api.openweathermap.org/data/2.5/weather?q=' + CGI.escape(location) + '&appid=' + ENV['API_KEY'])
-      end
+      @parsed_response = 
+      HTTParty.get('https://api.openweathermap.org/data/2.5/weather?q=' + CGI.escape(location) + '&appid=' + ENV['API_KEY'])
     end
   end
 end
