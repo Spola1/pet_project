8a1503d04cb51f59f7a700c9e38fdaf3890ba1a4
Spola1 <1111.22224@mail.ru>
2023-01-24 19:12:36 +0300

    update smtp settings for delivery mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,11 +16,22 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.default_url_options = { host: 'https://pet-project.fly.dev/' }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'https://pet-project.fly.dev/',
+    user_name:            "#{ENV['GMAIL_USERNAME']}",
+    password:             "#{ENV['GMAIL_PASSWORD']}",
+    authentication:       'plain',
+    enable_starttls_auto: true  
+ }
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
